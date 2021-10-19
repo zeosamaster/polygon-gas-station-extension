7c44f4abba6a8a5bc16e2bf4e46494edc3d80bb9
Fábio Matias <fabio_mcm2@hotmail.com>
2021-10-20 00:48:50 +0100

    feat: poll prices on popup

--- src/popup.js
+++ src/popup.js
@@ -1,17 +1,21 @@
 let url = "https://gpoly.blockscan.com/gasapi.ashx?apikey=key&method=gasoracle";
 
-// Force refreshes gas prices and displays details
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    chrome.browserAction.setBadgeText({
-      text: Number(data.result.ProposeGasPrice).toFixed(0),
+function getPrices() {
+  // Force refreshes gas prices and displays details
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      chrome.browserAction.setBadgeText({
+        text: Number(data.result.ProposeGasPrice).toFixed(0),
+      });
+      setValue("standard", data.result.SafeGasPrice);
+      setValue("fast", data.result.ProposeGasPrice);
+      setValue("instant", data.result.FastGasPrice);
     });
-    setValue("standard", data.result.SafeGasPrice);
-    setValue("fast", data.result.ProposeGasPrice);
-    setValue("instant", data.result.FastGasPrice);
-  });
+}
 
 function setValue(id, value) {
   document.getElementById(id).textContent = Number(value).toFixed(0);
 }
+
+setInterval(getPrices, 500);
